05a17134c169ac05a858b5542d7269cb061d32bd
thanhUni0708 <thanhtm78@akagames.io>
2023-11-03 10:24:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/audiobook/Login_screen.kt
@@ -0,0 +1,12 @@
+package com.example.audiobook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+@Suppress("ClassName")
+class Login_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+    }
+}
---
+++ app/src/main/java/com/example/audiobook/Login_screen_2.kt
@@ -0,0 +1,11 @@
+package com.example.audiobook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login_screen_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_screen2)
+    }
+}

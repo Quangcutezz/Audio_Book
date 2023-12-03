8becaa8a767a932cb409bd4d03d166c6ce07c5d9
thanhUni0708 <thanhtm78@akagames.io>
2023-12-03 11:19:22 +0700

    update code 11h19p 3/12/2023

--- app/src/main/java/com/example/audiobook/Sign_up.kt
+++ app/src/main/java/com/example/audiobook/Sign_up.kt
@@ -10,6 +10,8 @@ import android.widget.EditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseUser
+import android.widget.Toast
 class Sign_up : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
@@ -34,11 +36,52 @@ class Sign_up : AppCompatActivity() {
         passwordConfirm = findViewById(R.id.ConfirmPassword)
 
         signupButton = findViewById(R.id.signup)
+        signupButton.setOnClickListener {
+            signUp()
+        }
 
         val turn_back : ImageButton = findViewById(R.id.turn_back)
         turn_back.setOnClickListener{
             val intent = Intent(this,Login_screen_2::class.java)
             startActivity(intent)
         }
+    }
+    private fun signUp(){
+
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // Đăng ký tài khoản mới bằng email và mật khẩu
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Đăng ký thành công
+                    val user: FirebaseUser? = auth.currentUser
+                    if (user != null) {
+                        // Cập nhật thông tin tài khoản trên Firebase Authentication (nếu cần)
+                        // Ví dụ: user.updateEmail(newEmail)
+
+                        // Chuyển sang Home_page khi đăng ký thành công
+                        navigateToHomeActivity()
+                    } else {
+                        Log.e("SignUpActivity", "Current user is null")
+                    }
+                } else {
+                    // Đăng ký thất bại
+                    Log.w("SignUpActivity", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(this, "Registration failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun navigateToHomeActivity() {
+        val intent = Intent(this, Home_page::class.java)
+        startActivity(intent)
+        finish() // Đóng SignUpActivity sau khi chuyển sang HomeActivity
     }
 }

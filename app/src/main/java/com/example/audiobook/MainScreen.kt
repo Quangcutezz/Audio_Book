93b11c69866b283f935cbf53391c438ee38c7a04
thanhUni0708 <thanhtm78@akagames.io>
2023-12-05 20:28:24 +0700

    update code 20h28p 5/12/2023

--- app/src/main/java/com/example/audiobook/MainScreen.kt
+++ app/src/main/java/com/example/audiobook/MainScreen.kt
@@ -1,6 +1,7 @@
 package com.example.audiobook
 
 import android.os.Bundle
+import android.widget.TextView
 
 import androidx.appcompat.widget.Toolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -14,18 +15,27 @@ import com.example.audiobook.databinding.ActivityMainScreenBinding
 class MainScreen : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainScreenBinding
-
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        val toolbar: Toolbar = findViewById(R.id.toolbar2)
-//        setSupportActionBar(toolbar)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
         val navView: BottomNavigationView = binding.navView
+        textView = binding.title
 
 
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main_screen)
+
+        // Thiết lập text cho title ban đầu
+        updateTitle(navController.currentDestination?.id)
+
+        // Thêm listener để cập nhật title khi chuyển đổi fragment
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            updateTitle(destination.id)
+        }
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -36,5 +46,17 @@ class MainScreen : AppCompatActivity() {
 //        )
         //setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+    }
+    private fun updateTitle(destinationId: Int?) {
+        val titleResId = when (destinationId) {
+            R.id.navigation_home -> R.string.title_home
+            R.id.navigation_search -> R.string.title_search
+            R.id.navigation_music -> R.string.title_music
+            R.id.navigation_profile -> R.string.title_profile
+            else -> R.string.app_name // Mặc định khi không tìm thấy id nào khớp
+        }
+
+        textView.text = getString(titleResId)
     }
 }
